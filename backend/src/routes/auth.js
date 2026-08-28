13ed8cd4d088c7cf8713129a0b6eaf11ecaf43d4
Mahan-AC <mahanasadpour2005@gmail.com>
2026-08-28 13:01:55 +0330

    making jwtPrivateKey + validating login input

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,18 +2,9 @@ import express from "express"
 import Joi from "joi"
 import { emailExists, createUser  } from "../controllers/signup.js"
 import { authenticateUser } from "../controllers/login.js";
+import { generateAuthToken } from "../utils/jwt.js";
 const router = express.Router();
 
-
-function validate(req) {
-  const schema = Joi.object({
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(8).max(1024).required(),
-    full_name: Joi.string().min(3).max(50).required()
-  });
-
-  return schema.validate(req);
-}
 
 router.post('/signup', async (req, res) => {
     try {
@@ -24,13 +15,18 @@ router.post('/signup', async (req, res) => {
         
         if (await emailExists(email))  return res.status(400).send("Email already exists");
 
-        const user = await createUser(full_name, email, password);
+        const { user, userId } = await createUser(full_name, email, password);
 
-        res.status(201).json({
-        full_name: user.full_name,
-        email: user.email,
-        createdAt: user.created_at,
-        });
+        const token = generateAuthToken(userId, user.role);
+
+        res
+            .header("x-auth-token", token)
+            .status(201)
+            .json({
+                user_id: user.user_id,
+                full_name: user.full_name,
+                email: user.email
+            });
 
     } catch (error) {
         console.error("Error during signup:", error.message);
@@ -39,19 +35,25 @@ router.post('/signup', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
+    const { error } = validate(req.body, false);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const {email, password} = req.body;
 
     try {
-        const user = await authenticateUser(email, password);
+        const { user, userId } = await authenticateUser(email, password);
 
-        if (!user) return res.status(400).send("Invalid email or password");
+        if (!user) return res.status(401).send("Invalid email or password");
 
-        res.status(200).json({
-        full_name: user.full_name,
-        email: user.email,
-        role: user.role,
-        createdAt: user.created_at,
-        });
+        const token = generateAuthToken(userId, user.role);
+
+        res
+            .header("x-auth-token", token)
+            .json({
+                user_id: user.user_id,
+                full_name: user.full_name,
+                email: user.email
+            });
 
     } catch (error) {
         console.error("Error during login:", error.message);
@@ -64,5 +66,25 @@ router.post('/logout', async (_, res) => {
     // needs to be implemented with JWT or session management for proper logout functionality
 });
 
+
+function validate(req, mode=true) { // mode=true -> signup | mode=false -> login
+    let schema;
+    
+    if (mode) {
+        schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(1024).required(),
+        full_name: Joi.string().min(3).max(50).required()
+        });
+   }
+    else {
+        schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(1024).required(),
+        });
+    }
+
+  return schema.validate(req);
+}
 
 export default router;
