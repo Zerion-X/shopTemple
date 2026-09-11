a8be4cf650a7911f6356f0018ad96d244805d169
Ahmd415-lang <ahmd859097@gmail.com>
2026-09-11 22:29:30 +0330

    patch and delete for categories

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -3,13 +3,16 @@ import Joi from "joi"
 import { auth } from "../middleware/auth.js";
 import isAdmin from "../middleware/admin.js"
 import arcjetProtect from "../middleware/arcjet.js";
+import upload from "../middleware/upload.js";
+import { uploadToCloudinary } from "../lib/cloudinaryUpload.js"
+import cloudinary from "../lib/cloudinary.js"
 import { pool } from "../lib/db.js";
 const router = express.Router();
 
 
 router.get("/", arcjetProtect, async (req, res) => {
     try {
-        const [categories] = await pool.execute("SELECT * FROM categories");
+        const [categories] = await pool.execute("SELECT category_id, name, image_url FROM categories");
         res.json(categories);
     } catch (error) {
         console.error("Error fetching categories:", error);
@@ -17,20 +20,172 @@ router.get("/", arcjetProtect, async (req, res) => {
     }
 });
 
-router.post("/", arcjetProtect, auth, isAdmin, async (req, res) => {
+router.post("/", arcjetProtect, auth, isAdmin, upload.single("image"), async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     const { name } = req.body;
 
     try {
+        let imageUrl = null;
+        let imagePublicId = null;
+
+        if (req.file) {
+
+            const result = await uploadToCloudinary(
+                req.file.buffer,
+                "shop-temple/categories"
+            );
+
+            imageUrl = result.secure_url;
+            imagePublicId = result.public_id;
+        }
+
         const [result] = await pool.execute(
-            "INSERT INTO categories (name) VALUES (?)",
-            [name]
+            "INSERT INTO categories (name, image_url, image_public_id) VALUES (?, ?, ?)",
+            [name, imageUrl, imagePublicId]
         );
-        res.status(201).json({ id: result.insertId, name });
+        
+        const categoryId = result.insertId;
+        const [rows] = await pool.execute(
+            "SELECT * FROM categories WHERE category_id = ?",
+            [categoryId]
+        );
+        res.status(201).json(rows[0]);
     } catch (error) {
         console.error("Error creating category:", error);
         res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+router.patch("/:id", arcjetProtect, auth, isAdmin, upload.single("image"), async (req, res) => {
+    const categoryId = req.params.id;
+
+    try {
+        const [rows] = await pool.execute(
+            "SELECT * FROM categories WHERE category_id = ?",
+            [categoryId]
+        );
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                error: "category not found"
+            });
+        }
+
+        const category = rows[0];
+
+        if (Object.keys(req.body).length > 0) {
+            const { error } = validateUpdate(req.body);
+
+            if (error) {
+                return res.status(400).json({
+                    error: error.details[0].message
+                });
+            }
+        }
+
+        const fieldsToUpdate = {};
+
+        if (req.body.name !== undefined) {
+            fieldsToUpdate.name = req.body.name;
+        }
+
+        let oldImagePublicId = null;
+
+        if (req.file) {
+            const result = await uploadToCloudinary(
+                req.file.buffer,
+                "shop-temple/categories"
+            );
+
+            fieldsToUpdate.image_url = result.secure_url;
+            fieldsToUpdate.image_public_id = result.public_id;
+
+            oldImagePublicId = category.image_public_id;
+        }
+
+        if (Object.keys(fieldsToUpdate).length === 0) {
+            return res.status(400).json({
+                error: "No fields to update"
+            });
+        }
+
+        const setClause = Object.keys(fieldsToUpdate)
+            .map(field => `${field} = ?`)
+            .join(", ");
+
+        const values = Object.values(fieldsToUpdate);
+        values.push(categoryId);
+
+        await pool.execute(
+            `UPDATE categories
+                SET ${setClause}
+                WHERE category_id = ?`,
+            values
+        );
+
+        if (oldImagePublicId) {
+            await cloudinary.uploader.destroy(oldImagePublicId);
+        }
+
+        const [updatedRows] = await pool.execute(
+            `SELECT
+                category_id,
+                name,
+                image_url
+                FROM categories
+                WHERE category_id = ?`,
+            [categoryId]
+        );
+
+        return res.json(updatedRows[0]);
+    } catch (error) {
+        console.error("Error updating category:", error);
+
+        return res.status(500).json({
+            error: "Internal server error"
+        });
+    }
+});
+
+router.delete("/:id", arcjetProtect, auth, isAdmin, async (req, res) => {
+
+    const categoryId = req.params.id;
+
+    try {
+
+        const [rows] = await pool.execute(
+            "SELECT image_public_id FROM categories WHERE category_id = ?",
+            [categoryId]
+        );
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                error: "category not found"
+            });
+        }
+
+        const category = rows[0];
+
+        if (category.image_public_id) {
+            await cloudinary.uploader.destroy(
+                category.image_public_id
+            );
+        }
+
+        await pool.execute(
+            "DELETE FROM categories WHERE category_id = ?",
+            [categoryId]
+        );
+
+        return res.status(204).send();
+
+    } catch (error) {
+        console.error("Error deleting category:", error);
+
+        return res.status(500).json({
+            error: "Internal server error"
+        });
     }
 });
 
@@ -42,6 +197,14 @@ function validate(req) {
   });
 
   return schema.validate(req);
+}
+
+function validateUpdate(req) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(45)
+    });
+
+    return schema.validate(req);
 }
 
 export default router;
